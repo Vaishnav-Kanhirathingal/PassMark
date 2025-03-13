45c3da6464019760c3c724ed921bba40d19d56ae
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-03-14 00:51:45 +0530

    res added, horizontal preview annotation added, dims added

---
+++ app/src/main/java/easter/egg/passmark/data/shared/PassMarkDimensions.kt
@@ -0,0 +1,7 @@
+package easter.egg.passmark.data.shared
+
+import androidx.compose.ui.unit.dp
+
+object PassMarkDimensions {
+    val minTouchSize = 48.dp
+}
---
+++ app/src/main/java/easter/egg/passmark/utils/MobileHorizontalPreview.kt
@@ -0,0 +1,10 @@
+package easter.egg.passmark.utils
+
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(
+    widthDp = 780,
+    heightDp = 360,
+    showBackground = true
+)
+annotation class MobileHorizontalPreview
