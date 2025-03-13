6cc0856b224cb547df2102584852322cf09d066d
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-03-14 00:21:16 +0530

    fonts created

---
+++ app/src/main/java/easter/egg/passmark/data/shared/PassMarkFonts.kt
@@ -0,0 +1,55 @@
+package easter.egg.passmark.data.shared
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
+import easter.egg.passmark.R
+
+object PassMarkFonts {
+    object Display : SubStyleFonts {
+        override val small: TextUnit = 36.sp
+        override val medium: TextUnit = 45.sp
+        override val large: TextUnit = 57.sp
+    }
+
+    object Headline : SubStyleFonts {
+        override val small: TextUnit = 24.sp
+        override val medium: TextUnit = 28.sp
+        override val large: TextUnit = 32.sp
+    }
+
+    object Title : SubStyleFonts {
+        override val small: TextUnit = 14.sp
+        override val medium: TextUnit = 16.sp
+        override val large: TextUnit = 22.sp
+    }
+
+    object Body : SubStyleFonts {
+        override val small: TextUnit = 12.sp
+        override val medium: TextUnit = 14.sp
+        override val large: TextUnit = 16.sp
+    }
+
+    object Label : SubStyleFonts {
+        override val small: TextUnit = 11.sp
+        override val medium: TextUnit = 12.sp
+        override val large: TextUnit = 14.sp
+    }
+}
+
+private interface SubStyleFonts {
+    val small: TextUnit
+    val medium: TextUnit
+    val large: TextUnit
+}
+
+val RobotoFont = FontFamily(
+    Font(resId = R.font.roboto_thin, weight = FontWeight.Thin),
+    Font(resId = R.font.roboto_regular, weight = FontWeight.Normal),
+    Font(resId = R.font.roboto_medium, weight = FontWeight.Medium),
+    Font(resId = R.font.roboto_semi_bold, weight = FontWeight.SemiBold),
+    Font(resId = R.font.roboto_bold, weight = FontWeight.Bold),
+    Font(resId = R.font.roboto_extra_bold, weight = FontWeight.ExtraBold),
+)
