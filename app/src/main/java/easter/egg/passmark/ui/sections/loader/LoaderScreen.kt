62b1e3998389e99c85d6404ceb0de9ccbf0c736c
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-03-15 16:13:51 +0530

    UI changes

--- app/src/main/java/easter/egg/passmark/ui/sections/loader/LoaderScreen.kt
+++ app/src/main/java/easter/egg/passmark/ui/sections/loader/LoaderScreen.kt
@@ -1,11 +1,15 @@
 package easter.egg.passmark.ui.sections.loader
 
 import android.util.Log
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import easter.egg.passmark.data.shared.PassMarkDimensions
 import easter.egg.passmark.utils.MobileHorizontalPreview
 import easter.egg.passmark.utils.MobilePreview
 
@@ -29,7 +33,15 @@ object LoaderScreen {
                 }
             }
         )
-        CircularProgressIndicator()
+        Box(
+            modifier = modifier,
+            contentAlignment = Alignment.Center,
+            content = {
+                CircularProgressIndicator(
+                    modifier = Modifier.size(size = PassMarkDimensions.minTouchSize)
+                )
+            }
+        )
     }
 }
 
