4180c33d6cfc3db44fb5cfb7d23afdddd6215511
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-03-15 18:03:09 +0530

    Pre call state and docs added

--- app/src/main/java/easter/egg/passmark/utils/ScreenState.kt
+++ app/src/main/java/easter/egg/passmark/utils/ScreenState.kt
@@ -1,8 +1,17 @@
 package easter.egg.passmark.utils
 
+/** the screen state class which maintains the state of an api call. set the type `T` to `Unit` if no
+ * return is required
+ * */
 sealed class ScreenState<T> {
+    /** this is the state before any api call is made */
+    class PreCall<T> : ScreenState<T>()
+
     class Loading<T> : ScreenState<T>()
+
+    /** loaded state, contains the result of the output */
     class Loaded<T>(val result: T) : ScreenState<T>()
+
     sealed class ApiError<T> : ScreenState<T>() {
         class NetworkError<T> : ApiError<T>()
         class SomethingWentWrong<T> : ApiError<T>()
