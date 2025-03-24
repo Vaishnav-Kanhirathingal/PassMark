26a2d6b969cc0780eca99c5f144cf08077fa502c
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-03-25 02:56:31 +0530

    data handling changes

--- app/src/main/java/easter/egg/passmark/data/models/Password.kt
+++ app/src/main/java/easter/egg/passmark/data/models/Password.kt
@@ -1,8 +1,19 @@
 package easter.egg.passmark.data.models
 
+/** to be used to display stuff and only to be stored in memory */
 data class Password(
     val id: Int? = null,
     val vaultId: Int?,
+    val data: PasswordData,
+    val lastUsed: Long
+) {
+    fun toPasswordCapsule(key: String) :PasswordCapsule{
+        TODO("encrypt to string and create capsule")
+    }
+}
+
+/** sensitive content inside the password data */
+data class PasswordData(
     val title: String,
     val email: String,
     val userName: String,
@@ -11,4 +22,12 @@ data class Password(
     val notes: String,
     val useFingerPrint: Boolean,
     val saveToLocalOnly: Boolean
+)
+
+/** attack safe data with encrypted password data to be stored remotely / on-storage */
+data class PasswordCapsule(
+    val id: Int? = null,
+    val vaultId: Int?,
+    val data: String,
+    val lastUsed: Long?
 )
