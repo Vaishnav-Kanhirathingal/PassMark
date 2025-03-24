e4db2113a879b9d14552959806ab4a6a1e82c9a4
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-03-25 02:57:27 +0530

    major ui changes

--- app/src/main/java/easter/egg/passmark/ui/sections/user_edit/UserEditScreen.kt
+++ app/src/main/java/easter/egg/passmark/ui/sections/user_edit/UserEditScreen.kt
@@ -1,10 +1,14 @@
 package easter.egg.passmark.ui.sections.user_edit
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -12,14 +16,17 @@ import androidx.compose.material.icons.filled.Password
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.unit.dp
 import easter.egg.passmark.data.shared.PassMarkFonts
 import easter.egg.passmark.data.shared.setSizeLimitation
@@ -44,23 +51,27 @@ object UserEditScreen {
                 alignment = Alignment.CenterVertically
             ),
             content = {
+                val spacerModifier = Modifier
+                    .fillMaxWidth()
+                    .height(height = 16.dp)
+                Spacer(modifier = spacerModifier)
                 Text(
                     modifier = Modifier.fillMaxWidth(),
-                    text = if (isNewUser)"Create a Master Key" else "Confirm your master key",
+                    text = if (isNewUser) "Create a Master Key" else "Confirm your master key",
                     fontFamily = PassMarkFonts.font,
                     fontSize = PassMarkFonts.Headline.medium,
                 )
                 Text(
                     modifier = Modifier.fillMaxWidth(),
-                    text = "make sure to remember your master key. Without this, account recovery would be impossible",
+                    text = "Make sure to remember your master key. Without this, account recovery would be impossible",
                     fontFamily = PassMarkFonts.font,
                     fontSize = PassMarkFonts.Body.medium,
                 )
                 val errorText: String? =
                     viewModel.masterPasswordText.collectAsState().value.length.let {
                         when {
-                            it >= 32 -> "password should be less than 32 in length"
-                            it <= 8 -> "password should be more than 8 in length"
+                            it >= 32 -> "Password should be less than 32 in length"
+                            it <= 8 -> "Password should be more than 8 in length"
                             else -> null
                         }
                     }
@@ -70,7 +81,7 @@ object UserEditScreen {
                     onValueChange = viewModel::updateMasterPasswordText,
                     label = { Text(text = "Master Password") },
                     placeholder = { Text(text = "Secure Password") },
-                    isError = (errorText != null),
+                    isError = (!viewModel.showError.collectAsState().value || (errorText != null)),
                     supportingText = { Text(text = errorText ?: "Password is of correct length") },
                     leadingIcon = {
                         Icon(
@@ -92,16 +103,33 @@ object UserEditScreen {
                         )
                     }
                 )
-
                 Button(
                     modifier = Modifier
                         .setSizeLimitation()
                         .align(Alignment.End),
                     onClick = { TODO() },
                     content = {
-                        Text("Create")
+                        Box(
+                            modifier = Modifier,
+                            contentAlignment = Alignment.Center,
+                            content = {
+                                val isLoading =
+                                    viewModel.screenState.collectAsState().value.isLoading
+                                Text(
+                                    modifier = Modifier.alpha(alpha = if (isLoading) 0f else 1f),
+                                    text = "Create"
+                                )
+                                if (isLoading) {
+                                    CircularProgressIndicator(
+                                        modifier = Modifier.size(size = 24.dp),
+                                        color = MaterialTheme.colorScheme.onPrimary
+                                    )
+                                }
+                            }
+                        )
                     }
                 )
+                Spacer(modifier = spacerModifier)
             }
         )
     }
@@ -113,6 +141,7 @@ object UserEditScreen {
 fun UserEditScreenPreview() {
     UserEditScreen.Screen(
         modifier = Modifier.fillMaxSize(),
-        viewModel = UserEditViewModel()
+        viewModel = UserEditViewModel(),
+        isNewUser = true
     )
 }
--- app/src/main/java/easter/egg/passmark/ui/sections/user_edit/UserEditViewModel.kt
+++ app/src/main/java/easter/egg/passmark/ui/sections/user_edit/UserEditViewModel.kt
@@ -1,6 +1,7 @@
 package easter.egg.passmark.ui.sections.user_edit
 
 import androidx.lifecycle.ViewModel
+import easter.egg.passmark.utils.ScreenState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
@@ -16,4 +17,14 @@ class UserEditViewModel : ViewModel() {
     fun updateVisibility() {
         this._visible.value = !this._visible.value
     }
+
+    private val _showError: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val showError: StateFlow<Boolean> get() = _showError
+    fun updateShowError() {
+        this._showError.value = true
+    }
+
+    private val _screenState: MutableStateFlow<ScreenState<Unit>> =
+        MutableStateFlow(ScreenState.PreCall())
+    val screenState: StateFlow<ScreenState<Unit>> get() = _screenState
 }
