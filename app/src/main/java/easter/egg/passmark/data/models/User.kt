3b6084e81a701d82b9e876634de0f069d3d3854c
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-03-26 01:00:35 +0530

    serializable

--- app/src/main/java/easter/egg/passmark/data/models/User.kt
+++ app/src/main/java/easter/egg/passmark/data/models/User.kt
@@ -1,7 +1,11 @@
 package easter.egg.passmark.data.models
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class User(
-    val userId: String? = null,
-    val createdAt: String,
-    val personalCheckKey: String
+    @SerialName(value = "user_id") val userId: String? = null,
+    @SerialName(value = "created_at") val createdAt: String,
+    @SerialName(value = "personal_check_key") val personalCheckKey: String
 )
