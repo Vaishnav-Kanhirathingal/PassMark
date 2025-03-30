ffa2b276e6c8dd493310e8efbf7103f224c0e66f
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-03-31 01:30:48 +0530

    datastore created

---
+++ app/src/main/java/easter/egg/passmark/data/storage/PassMarkDataStore.kt
@@ -0,0 +1,54 @@
+package easter.egg.passmark.data.storage
+
+import android.content.Context
+import android.util.Log
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import easter.egg.passmark.utils.datastore
+import easter.egg.passmark.utils.security.KeyStoreHandler
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+
+class PassMarkDataStore(
+    private val context: Context,
+    private val authId: String
+) {
+    companion object {
+        private val passwordKey = stringPreferencesKey(name = "password_key")
+        private val initializationVectorKey = stringPreferencesKey(name = "i_v_key")
+    }
+
+    private val TAG = this::class.simpleName
+    private val keyStoreHandler = KeyStoreHandler(authId = authId).apply { this.initialize() }
+
+    //-----------------------------------------------------------------------------------------store
+    suspend fun savePassword(password: String) {
+        context.datastore.edit {
+            val (encryptedPassword, iv) = keyStoreHandler.encrypt(password)
+            it[passwordKey] = encryptedPassword
+            it[initializationVectorKey] = iv
+        }
+    }
+
+    //-----------------------------------------------------------------------------------------fetch
+    fun fetchPassword(): Flow<String?> {
+        return context.datastore.data
+            .catch { it.printStackTrace() }
+            .map {
+                try {
+                    val input = it[passwordKey]
+                    val iv = it[initializationVectorKey]
+                    if (input == null || iv == null) {
+                        Log.d(TAG, "input null = ${input == null}, iv null = ${iv == null}")
+                        null
+                    } else {
+                        keyStoreHandler.decrypt(input = input, iv = iv)
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    null
+                }
+            }
+    }
+}
--- app/src/main/java/easter/egg/passmark/utils/PassMarkApp.kt
+++ app/src/main/java/easter/egg/passmark/utils/PassMarkApp.kt
@@ -1,7 +1,13 @@
 package easter.egg.passmark.utils
 
 import android.app.Application
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class PassMarkApp : Application()
+
+val Context.datastore: DataStore<Preferences> by preferencesDataStore(name = "passmark_datastore")
