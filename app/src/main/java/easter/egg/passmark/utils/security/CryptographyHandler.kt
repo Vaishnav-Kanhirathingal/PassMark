0a9e6e55b885096dbf3781db1dd5727cdd235ca0
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-03-31 03:43:24 +0530

    encryption handler created

---
+++ app/src/main/java/easter/egg/passmark/utils/security/CryptographyHandler.kt
@@ -0,0 +1,66 @@
+package easter.egg.passmark.utils.security
+
+import easter.egg.passmark.data.models.User
+import io.ktor.util.decodeBase64Bytes
+import io.ktor.util.encodeBase64
+import java.security.MessageDigest
+import java.security.SecureRandom
+import javax.crypto.Cipher
+import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.SecretKeySpec
+
+class CryptographyHandler(
+    password: String,
+    initializationVector: ByteArray,
+) {
+    private val secretKeySpec = SecretKeySpec(
+        MessageDigest.getInstance("SHA-256")
+            .digest(password.toByteArray())
+            .copyOfRange(0, 16), "AES"
+    )
+    private val iv = IvParameterSpec(initializationVector)
+
+    private fun fetchCipher(): Cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+
+    fun encrypt(
+        input: String
+    ): String {
+        return fetchCipher()
+            .also { it.init(Cipher.ENCRYPT_MODE, secretKeySpec, iv) }
+            .doFinal(input.decodeBase64Bytes()).encodeBase64()
+    }
+
+    fun decrypt(
+        encryptedInput: String
+    ): String {
+        return fetchCipher()
+            .also { it.init(Cipher.DECRYPT_MODE, secretKeySpec, iv) }
+            .doFinal(encryptedInput.decodeBase64Bytes()).encodeBase64()
+    }
+
+    fun getEncryptedPuzzle(): String {
+        return encrypt(input = User.PUZZLE_SOLUTION)
+    }
+
+    fun isPuzzleCorrect(
+        apiProvidedPuzzle: String
+    ) {
+        (decrypt(apiProvidedPuzzle) == User.PUZZLE_SOLUTION)
+    }
+}
+
+fun main() {
+    val iv = ByteArray(16).also { SecureRandom().nextBytes(it) }
+    val cryptographyHandler = CryptographyHandler(
+        password = "some password",
+        initializationVector = iv
+    )
+
+    println(
+        cryptographyHandler.decrypt(
+            encryptedInput = cryptographyHandler.encrypt(
+                input = "sample"
+            )
+        )
+    )
+}
