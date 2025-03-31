b66b6a1860325f3eace4b23ab8c6b360783e649a
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-04-01 01:39:17 +0530

    user class updated

--- app/src/main/java/easter/egg/passmark/data/models/User.kt
+++ app/src/main/java/easter/egg/passmark/data/models/User.kt
@@ -6,6 +6,12 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class User(
     @SerialName(value = "user_id") val userId: String? = null,
-    @SerialName(value = "created_at") val createdAt: String,
-    @SerialName(value = "personal_check_key") val personalCheckKey: String
-)
+    @SerialName(value = "created_at") val createdAt: String? = null,
+    @SerialName(value = "password_puzzle_encrypted") val passwordPuzzleEncrypted: String,
+    @SerialName(value = "encryption_key_initialization_vector") val encryptionKeyInitializationVector: String
+) {
+    companion object {
+        const val PUZZLE_SOLUTION =
+            "gGR/5pKTUvgxRbtjNA38r00ArR4n6HVwtWKG3M/pakx+XNlZLtXphlInW9AwhmsI0edA6V+UJsdDQamREftnGS9CfZGVN22g4OdBz8vgV9slWjSE+Dte3EM11p0KVdN8MSUD5A"
+    }
+}
