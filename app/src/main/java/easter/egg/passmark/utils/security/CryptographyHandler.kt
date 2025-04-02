bd7323bdbfa19f30cb2bd0bd642ecde3f3dc57ef
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-04-03 01:40:28 +0530

    new constructors created to prevent errors

--- app/src/main/java/easter/egg/passmark/utils/security/CryptographyHandler.kt
+++ app/src/main/java/easter/egg/passmark/utils/security/CryptographyHandler.kt
@@ -8,16 +8,32 @@ import javax.crypto.Cipher
 import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
 
-class CryptographyHandler(
-    password: String,
-    initializationVector: ByteArray,
+// rename to password data encryption handler
+class CryptographyHandler private constructor(
+    private val secretKeySpec: SecretKeySpec,
+    private val ivSpec: IvParameterSpec
 ) {
-    private val secretKeySpec = SecretKeySpec(
-        MessageDigest.getInstance("SHA-256")
-            .digest(password.toByteArray())
-            .copyOfRange(0, 16), "AES"
+    constructor(
+        password: String,
+        initializationVector: String
+    ) : this(
+        password = password,
+        initializationVector = initializationVector.decodeBase64Bytes()
     )
-    private val iv = IvParameterSpec(initializationVector)
+
+    constructor(
+        password: String,
+        initializationVector: ByteArray
+    ) : this(
+        secretKeySpec = SecretKeySpec(
+            MessageDigest.getInstance("SHA-256")
+                .digest(password.toByteArray())
+                .copyOfRange(0, 16), "AES"
+        ),
+        ivSpec = IvParameterSpec(initializationVector)
+    )
+
+    val initializationVectorAsString: String get() = ivSpec.iv.encodeBase64()
 
     private fun fetchCipher(): Cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
 
@@ -25,7 +41,7 @@ class CryptographyHandler(
         input: String
     ): String {
         return fetchCipher()
-            .also { it.init(Cipher.ENCRYPT_MODE, secretKeySpec, iv) }
+            .also { it.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivSpec) }
             .doFinal(input.toByteArray()).encodeBase64()
     }
 
@@ -33,7 +49,7 @@ class CryptographyHandler(
         encryptedInput: String
     ): String {
         return fetchCipher()
-            .also { it.init(Cipher.DECRYPT_MODE, secretKeySpec, iv) }
+            .also { it.init(Cipher.DECRYPT_MODE, secretKeySpec, ivSpec) }
             .doFinal(encryptedInput.decodeBase64Bytes())
             .let { String(it) }
     }
@@ -42,7 +58,8 @@ class CryptographyHandler(
         return encrypt(input = User.PUZZLE_SOLUTION)
     }
 
-    fun checkValidation(
+    /** returns `true` if puzzle is solved, `false` otherwise */
+    fun solvePuzzle(
         apiProvidedEncryptedPuzzle: String
     ): Boolean = (decrypt(apiProvidedEncryptedPuzzle) == User.PUZZLE_SOLUTION)
 }
@@ -60,5 +77,18 @@ class CryptographyHandler(
 //                input = "sample"
 //            )
 //        )
+//    )
+//
+//    println("something else".toByteArray().encodeBase64().decodeBase64String())
+//    println(
+//        mutableListOf(83, 111, 109, 101, 116, 104, 105, 110, 103, 32, 101, 108, 115, 101)
+//            .map { it.toByte() }
+//            .toByteArray()
+//
+//            .encodeBase64() //string
+//
+//            .decodeBase64Bytes()
+//            .let { String(it) }
+//
 //    )
 //}
