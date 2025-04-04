a3bc56499bf375bdc0786a14af90d7e16bfb1015
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-04-05 02:58:31 +0530

    changes to apis

--- app/src/main/java/easter/egg/passmark/data/supabase/api/PasswordApi.kt
+++ app/src/main/java/easter/egg/passmark/data/supabase/api/PasswordApi.kt
@@ -1,27 +1,28 @@
 package easter.egg.passmark.data.supabase.api
 
 import easter.egg.passmark.data.models.content.Password
+import easter.egg.passmark.data.models.content.PasswordCapsule
 import easter.egg.passmark.utils.security.PasswordCryptographyHandler
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.postgrest.from
 import javax.inject.Inject
 
 class PasswordApi @Inject constructor(
-    private val supabaseClient: SupabaseClient
+    supabaseClient: SupabaseClient
 ) {
     private val table = supabaseClient.from("passwords")
 
-    suspend fun savePassword(
-        password: Password,
-        passwordCryptographyHandler: PasswordCryptographyHandler
-    ) = table.upsert(
-        value = password, // TODO: save password capsule
-        request = { select() }
-    )
+    suspend fun savePassword(passwordCapsule: PasswordCapsule): PasswordCapsule {
+        return table
+            .upsert(value = passwordCapsule, request = { select() })
+            .decodeSingle<PasswordCapsule>()
+    }
 
     suspend fun getPasswordList(
         passwordCryptographyHandler: PasswordCryptographyHandler
     ): List<Password> {
-        TODO()
+        return table.select().decodeList<PasswordCapsule>().map {
+            it.toPassword(passwordCryptographyHandler = passwordCryptographyHandler)
+        }
     }
 }
--- app/src/main/java/easter/egg/passmark/data/supabase/api/VaultApi.kt
+++ app/src/main/java/easter/egg/passmark/data/supabase/api/VaultApi.kt
@@ -2,12 +2,15 @@ package easter.egg.passmark.data.supabase.api
 
 import easter.egg.passmark.data.models.content.Vault
 import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.postgrest.from
 import javax.inject.Inject
 
 class VaultApi @Inject constructor(
     private val supabaseClient: SupabaseClient
 ) {
-    fun getVaultList():List<Vault> {
-        TODO()
+    private val table = supabaseClient.from(table = "vaults")
+
+    suspend fun getVaultList(): List<Vault> {
+        return table.select().decodeList<Vault>()
     }
 }
