28daee95bf974e160ca7902336cf3062714da35a
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-04-06 17:22:37 +0530

    icon list created

--- app/src/main/java/easter/egg/passmark/data/models/content/Vault.kt
+++ app/src/main/java/easter/egg/passmark/data/models/content/Vault.kt
@@ -1,15 +1,51 @@
 package easter.egg.passmark.data.models.content
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.DirectionsBike
+import androidx.compose.material.icons.filled.AccountBalance
+import androidx.compose.material.icons.filled.Apartment
+import androidx.compose.material.icons.filled.Group
+import androidx.compose.material.icons.filled.Handshake
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Public
+import androidx.compose.material.icons.filled.SensorOccupied
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.filled.Smartphone
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.tooling.preview.Preview
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Vault(
-    @SerialName("id") val id: Int,
+    @SerialName("id") val id: Int? = null,
     @SerialName("name") val name: String,
     @SerialName("user_id") val userId: String? = null,
     @SerialName("icon_choice") val iconChoice: Int
-)
+) {
+    companion object {
+        val iconList = listOf(
+            Icons.Default.Public,
+            Icons.Default.AccountBalance,
+            Icons.Default.Smartphone,
+            Icons.Default.Group,
+            Icons.Default.Apartment,
+            Icons.Default.Handshake,
+            Icons.Default.ShoppingCart,
+            Icons.Default.Settings,
+            Icons.Default.Lock,
+            Icons.Default.Star,
+            Icons.Default.SensorOccupied,
+            Icons.AutoMirrored.Filled.DirectionsBike
+        )
+    }
+}
 
 /** types of passwords -
  * devices
@@ -18,3 +54,23 @@ data class Vault(
  * banking
  * unimportant
  */
+@Composable
+@Preview(
+    widthDp = 100,
+    heightDp = 400,
+    showBackground = true
+)
+private fun IconList() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        content = {
+            Vault.iconList.forEach {
+                Icon(
+                    imageVector = it,
+                    contentDescription = null
+                )
+            }
+        }
+    )
+}
