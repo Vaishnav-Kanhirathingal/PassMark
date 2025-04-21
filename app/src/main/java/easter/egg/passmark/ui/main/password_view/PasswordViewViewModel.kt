bd02eff01cdefcb3ac5e4d40e3b01123427c3ec9
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-04-21 14:00:43 +0530

    viewmodel created

---
+++ app/src/main/java/easter/egg/passmark/ui/main/password_view/PasswordViewViewModel.kt
@@ -0,0 +1,40 @@
+package easter.egg.passmark.ui.main.password_view
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import easter.egg.passmark.data.supabase.api.PasswordApi
+import easter.egg.passmark.utils.ScreenState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class PasswordViewViewModel @Inject constructor(
+    val passwordApi: PasswordApi
+) : ViewModel() {
+    private val _deleteDialogVisibility: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val deleteDialogVisibility: StateFlow<Boolean> = _deleteDialogVisibility
+
+    fun setDeleteDialogVisibility(visibility: Boolean) {
+        _deleteDialogVisibility.value = visibility
+    }
+
+    private val _deleteDialogState: MutableStateFlow<ScreenState<Unit>> =
+        MutableStateFlow(ScreenState.PreCall())
+    val deleteDialogState: StateFlow<ScreenState<Unit>> = _deleteDialogState
+
+    fun delete(passwordId: Int) {
+        _deleteDialogState.value = ScreenState.Loading()
+        viewModelScope.launch {
+            try {
+                passwordApi.deletePassword(passwordId = passwordId)
+                ScreenState.Loaded(Unit)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                ScreenState.ApiError.fromException(e = e)
+            }.let { newState: ScreenState<Unit> -> _deleteDialogState.value = newState }
+        }
+    }
+}
