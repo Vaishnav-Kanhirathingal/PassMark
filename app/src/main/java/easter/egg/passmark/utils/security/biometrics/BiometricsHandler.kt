e79dc4c9d7d2be509e27cb4470a9134d6cf1755f
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-05-14 03:00:43 +0530

    new prompt handler created

--- app/src/main/java/easter/egg/passmark/utils/security/biometrics/BiometricsHandler.kt
+++ app/src/main/java/easter/egg/passmark/utils/security/biometrics/BiometricsHandler.kt
@@ -4,6 +4,7 @@ import androidx.biometric.BiometricPrompt
 import androidx.fragment.app.FragmentActivity
 
 object BiometricsHandler {
+    @Deprecated(message = "Deprecated due to less flexibility")
     fun performBiometricAuthentication(
         activity: FragmentActivity,
         onComplete: () -> Unit,
@@ -32,6 +33,43 @@ object BiometricsHandler {
                     super.onAuthenticationError(errorCode, errString)
                     showToast("An authentication error has occurred")
                     onComplete()
+                }
+            }
+        ).authenticate(
+            BiometricPrompt
+                .PromptInfo.Builder()
+                .setTitle("Authenticate")
+                .setSubtitle("Authenticate to copy password")
+                .setNegativeButtonText("Cancel")
+                .build()
+        )
+    }
+
+    enum class BiometricHandlerOutput { AUTHENTICATED, FAILED, ERROR }
+
+    fun performBiometricAuthentication(
+        activity: FragmentActivity,
+        onComplete: (BiometricHandlerOutput) -> Unit,
+    ) {
+        BiometricPrompt(
+            activity,
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                    super.onAuthenticationSucceeded(result)
+                    onComplete(BiometricHandlerOutput.AUTHENTICATED)
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    onComplete(BiometricHandlerOutput.FAILED)
+                }
+
+                override fun onAuthenticationError(
+                    errorCode: Int,
+                    errString: CharSequence
+                ) {
+                    super.onAuthenticationError(errorCode, errString)
+                    onComplete(BiometricHandlerOutput.ERROR)
                 }
             }
         ).authenticate(
