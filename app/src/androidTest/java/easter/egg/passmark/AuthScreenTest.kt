dc69da9cc84907609f1c4bc26f30ff4e65c93e19
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-05-19 02:51:13 +0530

    partial testing

--- app/src/androidTest/java/easter/egg/passmark/AuthScreenTest.kt
+++ app/src/androidTest/java/easter/egg/passmark/AuthScreenTest.kt
@@ -1,8 +1,11 @@
 package easter.egg.passmark
 
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import easter.egg.passmark.ui.auth.AuthActivity
 import org.junit.Rule
@@ -15,11 +18,29 @@ class AuthScreenTest {
     val composeRule = createAndroidComposeRule<AuthActivity>()
 
     /** use this test when the app has no data and user does not exist in remote database */
+    @OptIn(ExperimentalTestApi::class)
     @Test
     fun createANewUser() {
+        // login screen
+        composeRule.waitUntilAtLeastOneExists(
+            matcher = hasText(text = "Google"),
+            timeoutMillis = 5_000
+        )
         Thread.sleep(3_000)
         composeRule.onNodeWithText("Google").performClick()
-        Thread.sleep(10_000) // click account, loader loads, pushing the app to create master password screen
+        Thread.sleep(8_000)
+        // opens create new user screen
+        composeRule.waitUntilAtLeastOneExists(
+            matcher = hasText("Create"),
+            timeoutMillis = 5_000
+        )
 
+        composeRule.onNodeWithText(text = "Master Password").performTextInput("123456789")
+        composeRule.onNodeWithText(text = "Create").performClick()
+
+        composeRule.waitUntilAtLeastOneExists(
+            matcher = hasText(text = "Search Passwords"),
+            timeoutMillis = 5_000
+        )
     }
 }
