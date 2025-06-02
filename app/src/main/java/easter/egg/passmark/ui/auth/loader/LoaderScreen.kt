d633cfe8631850920472ed261c90eea6dbf44bb3
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-06-02 15:02:16 +0530

    major changes to error screen in LoaderScreen

--- app/src/main/java/easter/egg/passmark/ui/auth/loader/LoaderScreen.kt
+++ app/src/main/java/easter/egg/passmark/ui/auth/loader/LoaderScreen.kt
@@ -6,9 +6,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -26,6 +31,7 @@ import easter.egg.passmark.data.supabase.api.UserApi
 import easter.egg.passmark.di.supabase.SupabaseModule
 import easter.egg.passmark.ui.shared_components.CustomLoader
 import easter.egg.passmark.utils.ScreenState
+import easter.egg.passmark.utils.annotation.MobileHorizontalPreview
 import easter.egg.passmark.utils.annotation.MobilePreview
 import easter.egg.passmark.utils.values.PassMarkFonts
 import easter.egg.passmark.utils.values.setSizeLimitation
@@ -64,66 +70,84 @@ object LoaderScreen {
             contentAlignment = Alignment.Center,
             content = {
                 if (screenState is ScreenState.ApiError) {
-                    Column(
+                    ErrorScreen(
                         modifier = Modifier
-//                            .padding(horizontal = 32.dp)
-                            .clip(shape = RoundedCornerShape(size = 16.dp))
-                            .background(color = MaterialTheme.colorScheme.surfaceContainer)
-                            .width(width = 280.dp)
-                            .padding(all = 16.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.spacedBy(
-                            space = 4.dp,
-                            alignment = Alignment.CenterVertically
-                        ),
-                        content = {
-                            Text(
-                                textAlign = TextAlign.Center,
-                                text = when (screenState) {
-                                    is ScreenState.ApiError.NetworkError -> "Network error"
-                                    is ScreenState.ApiError.SomethingWentWrong -> "Something went wrong"
-                                },
-                                fontFamily = PassMarkFonts.font,
-                                fontSize = PassMarkFonts.Title.medium,
-                                fontWeight = FontWeight.SemiBold,
-                                color = MaterialTheme.colorScheme.onSurface
-                            )
-                            Text(
-                                textAlign = TextAlign.Center,
-                                text = when (screenState) {
-                                    is ScreenState.ApiError.NetworkError -> "A network call has failed. Please check your internet connection and try again."
-                                    is ScreenState.ApiError.SomethingWentWrong -> "Something went wrong. Please try again."
-                                },
-                                fontFamily = PassMarkFonts.font,
-                                fontSize = PassMarkFonts.Body.medium,
-                                lineHeight = PassMarkFonts.Body.medium,
-                                fontWeight = FontWeight.Medium,
-                                color = MaterialTheme.colorScheme.onSurface
-                            )
-
-                            Box(
-                                modifier = Modifier
-                                    .setSizeLimitation()
-                                    .clip(shape = RoundedCornerShape(size = 16.dp))
-                                    .background(color = MaterialTheme.colorScheme.primaryContainer)
-                                    .clickable(onClick = viewModel::forceVerify)
-                                    .padding(horizontal = 16.dp),
-                                contentAlignment = Alignment.Center,
-                                content = {
-                                    Text(
-                                        text = "Try again",
-                                        fontFamily = PassMarkFonts.font,
-                                        fontSize = PassMarkFonts.Body.medium,
-                                        fontWeight = FontWeight.SemiBold,
-                                        color = MaterialTheme.colorScheme.onPrimaryContainer
-                                    )
-                                }
-                            )
-                        }
+                            .widthIn(max = 450.dp)
+                            .fillMaxWidth()
+                            .padding(horizontal = 24.dp),
+                        screenState = screenState,
+                        onRetry = viewModel::forceVerify
                     )
                 } else {
                     CustomLoader.FullScreenLoader(modifier = Modifier)
                 }
+            }
+        )
+    }
+
+    @Composable
+    fun <T> ErrorScreen(
+        modifier: Modifier,
+        screenState: ScreenState.ApiError<T>,
+        onRetry: () -> Unit
+    ) {
+        Column(
+            modifier = modifier
+                .clip(shape = RoundedCornerShape(size = 16.dp))
+                .background(color = MaterialTheme.colorScheme.surfaceContainer)
+                .padding(all = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(
+                space = 4.dp,
+                alignment = Alignment.CenterVertically
+            ),
+            content = {
+                Icon(
+                    modifier = Modifier.size(60.dp),
+                    imageVector = Icons.Default.Warning,
+                    contentDescription = null
+                )
+                Text(
+                    textAlign = TextAlign.Center,
+                    text = when (screenState) {
+                        is ScreenState.ApiError.NetworkError -> "Network error"
+                        is ScreenState.ApiError.SomethingWentWrong -> "Something went wrong"
+                    },
+                    fontFamily = PassMarkFonts.font,
+                    fontSize = PassMarkFonts.Title.medium,
+                    fontWeight = FontWeight.SemiBold,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+                Text(
+                    textAlign = TextAlign.Center,
+                    text = when (screenState) {
+                        is ScreenState.ApiError.NetworkError -> "A network call has failed. Please check your internet connection and try again."
+                        is ScreenState.ApiError.SomethingWentWrong -> "Something went wrong. Please check your internet connection and try again."
+                    },
+                    fontFamily = PassMarkFonts.font,
+                    fontSize = PassMarkFonts.Body.medium,
+                    lineHeight = PassMarkFonts.Body.medium,
+                    fontWeight = FontWeight.Medium,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+                Box(
+                    modifier = Modifier
+                        .setSizeLimitation()
+                        .clip(shape = RoundedCornerShape(size = 16.dp))
+                        .background(color = MaterialTheme.colorScheme.primaryContainer)
+                        .clickable(onClick = onRetry)
+                        .padding(horizontal = 16.dp),
+                    contentAlignment = Alignment.Center,
+                    content = {
+                        Text(
+                            text = "Try again",
+                            fontFamily = PassMarkFonts.font,
+                            fontSize = PassMarkFonts.Body.medium,
+                            fontWeight = FontWeight.SemiBold,
+                            color = MaterialTheme.colorScheme.onPrimaryContainer
+                        )
+                    }
+                )
             }
         )
     }
@@ -143,5 +167,25 @@ fun LoaderScreenPreview() {
         toMainActivity = {},
         toLoginScreen = {},
         toMasterKeyScreen = {}
+    )
+}
+
+@MobilePreview
+@MobileHorizontalPreview
+@Composable
+fun ErrorScreenPreview() {
+    Box(
+        modifier = Modifier.fillMaxWidth(),
+        contentAlignment = Alignment.Center,
+        content = {
+            LoaderScreen.ErrorScreen(
+                modifier = Modifier
+                    .widthIn(max = 360.dp)
+                    .fillMaxWidth()
+                    .padding(horizontal = 24.dp),
+                screenState = ScreenState.ApiError.SomethingWentWrong<Unit>(),
+                onRetry = {}
+            )
+        }
     )
 }
