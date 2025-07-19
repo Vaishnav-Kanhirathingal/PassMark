50d29131eed68e7ff0068962fdc40b6f915756d6
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-07-19 21:38:28 +0530

    PasswordEditDescribable created

---
+++ app/src/main/java/easter/egg/passmark/utils/accessibility/screens/PasswordEditDescribable.kt
@@ -0,0 +1,25 @@
+package easter.egg.passmark.utils.accessibility.screens
+
+import easter.egg.passmark.utils.accessibility.Describable
+
+enum class PasswordEditDescribable(override val desc: String) : Describable {
+    DISMISS(desc = "Cancel"),
+    SELECT_VAULT_BUTTON(desc = "Select Vault"),
+    SELECT_VAULT_DIALOG_CHOOSE_VAULT_TITLE(desc = "Select Vault"),
+    SAVE_BUTTON(desc = "Save Password"),
+    TITLE_TEXT_FIELD(desc = "Title"),
+    EMAIL_TEXT_FIELD(desc = "Email"),
+    USER_NAME_TEXT_FIELD(desc = "User name"),
+    PASSWORD_TEXT_FIELD(desc = "Password to save"),
+    WEBSITE_TEXT_FIELD(desc = "service's website URL"),
+    NOTES_TEXT_FIELD(desc = "Notes"),
+    USE_FINGERPRINT_SWITCH(desc = "Use fingerprint"),
+    KEEP_LOCAL_SWITCH(desc = "Save locally");
+
+    companion object {
+        fun getVaultTestTag(vaultName: String): Describable = object : Describable {
+            override val desc: String
+                get() = "Vault $vaultName"
+        }
+    }
+}
